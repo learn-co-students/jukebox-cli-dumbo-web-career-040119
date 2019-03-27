26b776e4cb2139a517ce0efee0d26ef81c42eec8
ftse105 <ftse105@gmail.com>
2019-03-27 19:44:45 -0400

    Done.

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -13,33 +13,65 @@
 # }
 
 def help
-  #this method should be the same as in jukebox.rb
+  help = <<-HELP
+I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program
+HELP
 
+  puts help
 end
 
+help
 
-
-def list(my_songs)
-  #this method is different! Collect the keys of the my_songs hash and 
-  #list the songs by name
+def list(songs)
+  songs.each_with_index { |item, index|
+    puts "#{index+1}. #{item}" }
 end
 
+list(songs)
 
-def play(my_songs)
-  #this method is slightly different!
-  #you should still ask the user for input and collect their song choice
-  #this time, only allow user's to input a song name
-  #check to see if the name they give is in fact a key of the my_songs hash
-  #if it isn't, tell them their choice is invalid
-  #if it is, play the song using the system 'open <file path>' syntax
-  #get the file path of the song by looking it up in the my_songs hash
-  
+def play(songs)
+  puts "Please enter a song name or number:"
+  user_response = gets.downcase.chomp
+
+  if (1..9).to_a.include?(user_response.to_i)
+    puts "Playing #{songs[user_response.to_i - 1]}"
+    elsif songs.include?(user_response)
+    puts "Playing #{user_response}"
+  else
+    puts "Invalid input, please try again"
+  end
 end
+
+play(songs)
 
 def exit_jukebox
-  #this method is the same as in jukebox.rb
+  puts "Goodbye"
 end
 
-def run(my_songs)
-  #this method is the same as in jukebox.rb
-end
+def run(songs)
+  #help
+  command = ""
+  while command
+  puts "Please enter a command:"
+  command = gets.downcase.strip
+  case command
+    when 'list'
+      list(songs)
+      when 'play'
+        list(songs)
+        play(songs)
+      when 'help'
+        help
+      when 'exit'
+        exit_jukebox
+        break
+      else
+        help
+      end
+    end
+  end
+  run(songs)
--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,59 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  help = <<-HELP
+I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program
+HELP
+puts help
+end
+
+
+def list(songs)
+  songs.each_with_index { |item, index|
+    puts "#{index + 1}. #{item}" }
+end
+
+def play(songs)
+  list(songs)
+  get_song = gets.chomp
+  if (0...songs.length).to_a.map(&:to_s).include?(get_song)
+    puts "#{songs[get_song.to_i]}"
+  elsif songs.include?(get_song)
+    puts "#{songs[songs.index(get_song)]}"
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  command = ""
+  while command
+    puts "Please enter a command:"
+  command = gets.downcase.strip
+
+  case command
+    when 'list'
+      list(songs)
+      when 'play'
+        list(songs)
+        play(songs)
+      when 'help'
+        help
+      when 'exit'
+        exit_jukebox
+        break
+      else
+        help
+      end
+    end
+  end
